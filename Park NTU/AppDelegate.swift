7605a12c247bdcc196228044f7f8aae77a37f5be
MR.Robot 💀 <JCassio@joselsons-macbook-pro-2.local>
2018-11-11 03:02:41 +0000

    Initial Commit

---
+++ Park NTU/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Park NTU
+//
+//  Created by MR.Robot 💀 on 12/02/2018.
+//  Copyright © 2018 PPM. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 3.0)
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+
+}
+
